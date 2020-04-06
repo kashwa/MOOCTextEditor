e8aabfaf2016ee139da8b785a8374b7195891fc0
Aabed Kashwa <kashwa80@gmail.com>
2020-04-06 19:01:41 +0200

    Assignment: Week-1 #words #sentences #syllables

--- src/document/BasicDocument.java
+++ src/document/BasicDocument.java
@@ -34,9 +34,9 @@ public class BasicDocument extends Document
 	@Override
 	public int getNumWords()
 	{
-		//TODO: Implement this method in week 2 according to the comments above.  
-		// See the Module 2 support videos if you need help.
-	    return 0;
+		List<String> words = getTokens("[a-zA-Z]+");
+		
+		return words.size();
 	}
 	
 	/**
@@ -54,9 +54,9 @@ public class BasicDocument extends Document
 	@Override
 	public int getNumSentences()
 	{
-	    //TODO: Implement this method.  See the Module 2 support videos 
-        // if you need help.
-        return 0;
+	    List<String> sentences = getTokens("[^.!?]+");
+	    
+	    return sentences.size();
 	}
 	
 	/**
@@ -76,12 +76,12 @@ public class BasicDocument extends Document
 	@Override
 	public int getNumSyllables()
 	{
-	    //TODO: Implement this method in week 2.  See the Module 2 support videos 
-        // if you need help.  And note that there is no need to use a regular
-		// expression for the syllable counting.  We recommend you implement 
-		// the helper function countSyllables in Document.java using a loop, 
-		// and then call it here on each word.
-        return 0;
+	    List<String> tokens = getTokens("[a-zA-Z]+");
+	    int totalSyllables = 0;
+	    for (String word : tokens) {
+	    	totalSyllables += countSyllables(word);
+	    }
+	    return totalSyllables;
 	}
 	
 	
